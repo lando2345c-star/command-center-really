3023e242a11c801bb7196c4077561f43760dba43
lando2345c-star <lando2345c@gmail.com>
2026-05-31 18:14:52 -0400

    Update facebook.js

--- api/facebook.js
+++ api/facebook.js
@@ -1,8 +1,14 @@
-export default async function handler(req, res) {
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
   const { pageId, token } = req.query;
-  const url = `https://graph.facebook.com/v19.0/${pageId}/insights?metric=page_impressions_unique,page_post_engagements,page_fans&period=day&access_token=${token}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json(data);
-}
+  try {
+    const url = `https://graph.facebook.com/v19.0/${pageId}/insights?metric=page_impressions_unique,page_post_engagements,page_fans&period=day&access_token=${token}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json(data);
+  } catch(e) {
+    res.status(500).json({ error: e.message });
+  }
+};
